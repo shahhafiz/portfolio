b75614a220ea229c5efc0262bf6f177043490c80
shahhafiz <iamshahhafiz@gmail.com>
2020-09-18 16:37:46 +0800

    Added shadow to header

--- components/header.js
+++ components/header.js
@@ -1,22 +1,41 @@
+import { useEffect, componentDidMount } from "react"
 import Head from 'next/head'
-export default function Component() {
+export default function Header() {
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll)
+
+    }, [])
+
+    function handleScroll() {
+        const scrollY = window.scrollY
+        console.log(scrollY)
+        if (scrollY < 50){
+            document.querySelector("#navbar").classList.remove("shadow-sm")
+           
+        } else if (scrollY > 50 ){
+            document.querySelector("#navbar").classList.add("shadow-sm")
+           
+        }
+    }
+
     return (
         <>
             <Head><title>Shah Al-Hafiz</title></Head>
-            <div class="flex w-full px-5 fixed">{/* container */}
-                <div class="w-full mt-5 flex items-center">
+            <div id="navbar" class="flex w-full px-5 fixed bg-white">
+                <div class="w-full my-5 flex items-center">
                     {/* Now available */}
                     <div class="w-40 py-2 px-4 bg-blue-100 rounded-full flex justify-center items-center">
                         <span class="text-blue-400 hover:text-blue-500">Now available</span>
                     </div>
-                    
+
                     {/* Hamburger */}
                     <div class="ml-auto md:hidden cursor-pointer">
                         <div class="h-1 w-8 mb-1 bg-gray-300 rounded"></div>
                         <div class="h-1 w-8 mb-1 bg-gray-300 rounded"></div>
                         <div class="h-1 w-8 mb-1 bg-gray-300 rounded"></div>
                     </div>
-                    
+
                     {/* Navigation */}
                     <div class="hidden md:block ml-auto">
                         <ul class="flex justify-end">
@@ -33,7 +52,7 @@ export default function Component() {
                                 <a class="text-gray-500 hover:text-blue-500" href="#">Contact</a>
                             </li>
                         </ul>
-                </div>
+                    </div>
                 </div>
             </div>
         </>
