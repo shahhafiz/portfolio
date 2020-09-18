6dd4ef06c0571fde1a0f5a5f584d1ed55251ee50
shahhafiz <iamshahhafiz@gmail.com>
2020-09-18 16:39:26 +0800

    Change to Web Developer

--- components/home.js
+++ components/home.js
@@ -4,7 +4,7 @@ export default function Component() {
             <img class="shadow-xl rounded-full w-48" src="employee.svg" />
             <div class="mt-5 text-2xl text-blue-500">Hi there, I'm</div>
             <div class="text-4xl font-semibold">&lt; Shah Al-Hafiz /&gt;</div>
-            <div class="text-2xl text-gray-500">Full Stack Developer</div>
+            <div class="text-2xl text-gray-500">Web Developer</div>
         </div>
     )
 }
