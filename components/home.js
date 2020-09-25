e3f2b75eecb0b5a6660b963b6168584eeaf945f4
shahhafiz <iamshahhafiz@gmail.com>
2020-09-25 16:35:20 +0800

    Use justify-center instead of pt-40

--- components/home.js
+++ components/home.js
@@ -1,6 +1,6 @@
 export default function Component() {
     return (
-        <div class="h-screen pt-40 flex flex-col justify-start items-center">
+        <div class="h-screen flex flex-col justify-center items-center">
             <img class="shadow-xl rounded-full w-48" src="employee.svg" />
             <div class="mt-5 text-2xl text-blue-500">Hi there, I'm</div>
             <div class="text-4xl font-semibold">&lt; Shah Al-Hafiz /&gt;</div>
