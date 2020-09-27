fe5aec1f15ef03005795ae39fb69cb704971335f
shahhafiz <iamshahhafiz@gmail.com>
2020-09-27 15:24:22 +0800

    Refactor work.js

--- pages/work/work.js
+++ pages/work/work.js
@@ -1,45 +1,67 @@
 import Card from '../../components/card'
 export default function Component() {
+
+    const works = [
+        {
+            title: "Shopum",
+            description: "An e-commerce app  which specifically catered for the students of University of Malaya to sell their second  hand or preloved items",
+            hashtags: "#ionic #firebase",
+            visual: (
+                <div className="flex justify-center items-end bg-indigo-500 text-center text-white text-2xl py-24" >
+                    Shopum
+                </div >
+            )
+        },
+        {
+            title: "Shopum Admin",
+            description: "Admin portal to monitor the users’ behavior and to display informative statistics of ShopumAdmin portal to monitor the users’ behavior and to display informative statistics of Shopum",
+            hashtags: "#ionic #firebase",
+            visual: (
+                <div className="flex justify-center items-end bg-blue-500 text-white text-2xl py-24">
+                    Shopum
+                    <div className="text-sm transform -translate-y-1">Admin</div>
+                </div>
+            )
+        },
+        {
+            title: "What The Food",
+            description: "Native Android mobile app that use image recognition to identify items in wet market using machine learning from Firebase",
+            hashtags: "#ionic #firebase",
+            visual: (
+                <div className="flex justify-center items-end bg-green-500 text-center text-white text-2xl py-24">
+                    What The Food
+                </div>
+            )
+        },
+        {
+            title: "shahhafiz.com",
+            description: "An online web portfolio to showcase my experiences, competencies, and skill sets as well as to attract potential employer",
+            hashtags: "#ionic #firebase",
+            visual: (
+                <div className="flex justify-center items-end bg-teal-500 text-center text-white text-2xl py-24">
+                    shahhafiz.com
+                </div>
+            )
+        }
+    ]
+
+
     return (
         <div className={"w-full lg:h-screen flex flex-col justify-center"}>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 pt-10">
-                <Card
-                    title={"Shopum"}
-                    description={"An e-commerce app  which specifically catered for the students of University of Malaya to sell their second  hand or preloved items"}
-                    hashtags={"#ionic #firebase"}
-                >
-                    <div className="flex justify-center items-end bg-indigo-500 text-center text-white text-2xl py-24">
-                        Shopum
-                </div>
-                </Card>
-                <Card
-                    title={"Shopum Admin"}
-                    description={"Admin portal to monitor the users’ behavior and to display informative statistics of ShopumAdmin portal to monitor the users’ behavior and to display informative statistics of Shopum"}
-                    hashtags={"#ionic #firebase"}
-                >
-                    <div className="flex justify-center items-end bg-blue-500 text-white text-2xl py-24">
-                        Shopum
-                            <div className="text-sm transform -translate-y-1">Admin</div>
-                    </div>
-                </Card>
-                <Card
-                    title={"What The Food"}
-                    description={"Native Android mobile app that use image recognition to identify items in wet market using machine learning from Firebase"}
-                    hashtags={"#ionic #firebase"}
-                >
-                    <div className="flex justify-center items-end bg-green-500 text-center text-white text-2xl py-24">
-                        What The Food
-                      </div>
-                </Card>
-                <Card
-                    title={"shahhafiz.com"}
-                    description={"An online web portfolio to showcase my experiences, competencies, and skill sets as well as to attract potential employer"}
-                    hashtags={"#ionic #firebase"}
-                >
-                   <div className="flex justify-center items-end bg-teal-500 text-center text-white text-2xl py-24">
-                        shahhafiz.com
-                      </div>
-                </Card>
+                {
+                    works.map(work => {
+                        return (
+                            <Card
+                                title={work.title}
+                                description={work.description}
+                                hashtags={work.hashtags}
+                            >
+                                {work.visual}
+                            </Card>
+                        )
+                    })
+                }
             </div>
         </div>
     )
