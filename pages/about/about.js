3a127aadc59451110f575f47b2cf95f139096d02
shahhafiz <iamshahhafiz@gmail.com>
2020-09-28 16:25:18 +0800

    Updated about text font-size on small screen

--- pages/about/about.js
+++ pages/about/about.js
@@ -2,7 +2,7 @@ export default function Component() {
     return (
         <div className="w-full h-screen flex flex-col justify-center">
             <div className="font-bold text-xl">&#128075; Hello, I'm Shah</div>
-            <div className="font-normal text-gray-600 leading-10 tracking-wide text-2xl text-justify">
+            <div className="font-normal text-gray-600 leading-10 tracking-wide text-lg md:text-2xl text-justify">
                 a web developer who recently just graduated from University of Malaya with a degree in Computer Science. I have passion in web development as well  as (sometimes) mobile app development.
                 I love to explore, experiment and learn new things which allow me to chase the fast pace technologies evolutions.
                 I’m looking forward a career that will allow me to enjoy working in a friendly and balanced environment and at the same time I able to express my passion.
