32ed5c69a0051bc0804c4632240ea6d61603a033
shahhafiz <iamshahhafiz@gmail.com>
2020-09-29 01:47:20 +0800

    Added about

--- pages/about/about.js
+++ pages/about/about.js
@@ -1,11 +1,17 @@
 export default function Component() {
     return (
         <div id="About" className="w-full h-screen flex flex-col justify-center">
-            <div className="font-bold text-xl">&#128075; Hello, I'm Shah</div>
+            {/* <div className="font-bold text-xl " className="animate-bounce"> */}
+            <span className="flex">
+                <span className="animate-bounce w-6 h-6"> &#128075; </span>
+                <span className="font-bold text-xl">Hello, I'm Shah</span>
+            </span>
+
+            {/* </div> */}
             <div className="font-normal text-gray-600 leading-10 tracking-wide text-lg md:text-2xl text-justify">
-                a web developer who recently just graduated from University of Malaya with a degree in Computer Science. I have passion in web development as well  as (sometimes) mobile app development.
-                I love to explore, experiment and learn new things which allow me to chase the fast pace technologies evolutions.
-                I’m looking forward a career that will allow me to enjoy working in a friendly and balanced environment and at the same time I able to express my passion.
+                a web developer who recently just graduated from University of Malaya with a degree in Computer Science. I have passion in web development as well  as (sometimes) in mobile app development.
+                I love to explore, experiment and learn new things which allow me to catch up to the high-speed technologies evolutions.
+                I’m looking forward opportunities that will allow me to enjoy working in a friendly and balanced environment.
             </div>
         </div>
     )
