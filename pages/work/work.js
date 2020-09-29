cb59a956a902c42346992fa62637fda8a04c18ec
shahhafiz <iamshahhafiz@gmail.com>
2020-09-29 15:13:36 +0800

    Modified work page

--- pages/work/work.js
+++ pages/work/work.js
@@ -1,10 +1,9 @@
-import Card from '../../components/card'
 export default function Component() {
-
     const works = [
         {
             title: "Shopum",
             description: "An e-commerce app  which specifically catered for the students of University of Malaya to sell their second  hand or preloved items",
+            link: "https://github.com/shahhafiz/Shopum2",
             hashtags: "#ionic #firebase",
             visual: (
                 <div className="flex justify-center items-end bg-indigo-500 text-center text-white text-2xl py-24" >
@@ -13,19 +12,9 @@ export default function Component() {
             )
         },
         {
-            title: "Shopum Admin",
-            description: "Admin portal to monitor the users’ behavior and to display informative statistics of ShopumAdmin portal to monitor the users’ behavior and to display informative statistics of Shopum",
-            hashtags: "#ionic #firebase",
-            visual: (
-                <div className="flex justify-center items-end bg-blue-500 text-white text-2xl py-24">
-                    Shopum
-                    <div className="text-sm transform -translate-y-1">Admin</div>
-                </div>
-            )
-        },
-        {
             title: "What The Food",
             description: "Native Android mobile app that use image recognition to identify items in wet market using machine learning from Firebase",
+            link: "https://github.com/shahhafiz/WhatTheFood",
             hashtags: "#android #firebase",
             visual: (
                 <div className="flex justify-center items-end bg-green-500 text-center text-white text-2xl py-24">
@@ -36,6 +25,7 @@ export default function Component() {
         {
             title: "shahhafiz.com",
             description: "An online web portfolio to showcase my experiences, competencies, and skill sets as well as to attract potential employer",
+            link: "https://github.com/shahhafiz/portfolio",
             hashtags: "#next.js #tailwindcss",
             visual: (
                 <div className="flex justify-center items-end bg-teal-500 text-center text-white text-2xl py-24">
@@ -48,22 +38,45 @@ export default function Component() {
 
     return (
         <div id="Work" className={"w-full lg:h-screen flex flex-col justify-center"}>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 pt-10">
+            <div className="flex flex-wrap justify-center pt-20 lg:pt-10">
                 {
-                    works.map((work,key) => {
+                    works.map((work, key) => {
                         return (
                             <Card
                                 key={key}
-                                title={work.title}
-                                description={work.description}
-                                hashtags={work.hashtags}
-                            >
-                                {work.visual}
-                            </Card>
+                                work={work}
+                            /> 
                         )
                     })
                 }
             </div>
+        </div>
+    )
+}
+
+// Card component
+function Card(props) {
+    const { work } = props
+    
+    return (
+        <div className="max-w-xs flex flex-col m-2 cursor-default">
+            <div className="h-full flex flex-col bg-gray-200 shadow rounded-lg z-10">
+                <div className="text-center text-gray-600 my-5">{work.title}</div>
+                {work.visual}
+                <div className={"flex flex-col justify-between text-center text-gray-600 p-5 flex-grow"}>
+                    <div>{work.description}</div>
+                    <a href={work.link} target="_blank" className="relative top-5">
+                        <span className="group cursor-pointer">
+                            <img src="github.png" alt="" className="more w-5 m-2 inline-flex animate-wiggle" />
+                            <span className="text-xs bg-white p-1 rounded border border-black border-opacity-25 group-hover:border-blue-400 group-hover:text-blue-400">Visit repo</span>
+                        </span>
+                    </a>
+                </div>
+            </div>
+            <div className="bg-blue-100 text-gray-700 h-8 text-center transform -translate-y-1 flex justify-center items-end rounded-b-lg shadow-lg">
+                {work.hashtags}
+            </div>
+            
         </div>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,19 +4,31 @@ module.exports = {
     removeDeprecatedGapUtilities: true,
   },
   purge: {
-    layers: ['utilities','components'],
-    content: ['./pages/**/*.js','./components/*.js'],
+    layers: ['utilities', 'components'],
+    content: ['./pages/**/*.js', './components/*.js'],
   },
   theme: {
     extend: {
       fontSize: {
-        
+
       },
       screens: {
-        
+
+      },
+      animation:{
+        wiggle: 'wiggle 1s ease-in-out infinite'
+      },
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-7deg)' },
+          '50%': { transform: 'rotate(7deg)' },
+        }
       }
     },
   },
-  variants: {},
+  variants: {
+    borderColor: ['group-hover'],
+    textColor: ['group-hover'],
+  },
   plugins: [],
 }
